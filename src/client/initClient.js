46cfeceb50e2add1136464e483e0faa1f5329290
devboell <devboell@gmail.com>
2017-07-12 16:03:32 +0200

    changed networkInterface to work on heroku

--- src/client/initClient.js
+++ src/client/initClient.js
@@ -3,7 +3,7 @@ import { ApolloClient, createNetworkInterface } from 'react-apollo'
 
 export default new ApolloClient({
   networkInterface: createNetworkInterface({
-    uri: 'http://localhost:3000/graphql',
+    uri: '/graphql',
     opts: {
       credentials: 'same-origin',
       // Pass headers here if your graphql server requires them
