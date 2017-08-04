4e8a676c254b69580c9560b9cff0849bb195a2bc
devboell <devboell@gmail.com>
2017-08-04 13:56:55 +0200

    using import resolver in import statement

--- src/client/components/EditorControls/index.js
+++ src/client/components/EditorControls/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
 
-import { editModes } from '../../containers/EditorPage/constants'
+import { editModes } from 'containers/EditorPage/constants'
 
 import Wrapper from './Wrapper'
 import Button from './Button'
