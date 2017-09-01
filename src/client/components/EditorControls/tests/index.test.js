004e591f79979741e242bf8988d728ee2b05d886
devboell <devboell@gmail.com>
2017-09-01 09:38:25 +0200

    refactor - code cleanup

--- src/client/components/EditorControls/tests/index.test.js
+++ src/client/components/EditorControls/tests/index.test.js
@@ -2,14 +2,11 @@ import React from 'react'
 import EditorControls from '../index'
 import Button from '../Button'
 
+
 const props = {
-  editMode: 'create',
-  selectedPerson: { id: 1 },
-  onCreatePerson: jest.fn(),
+  onPrepareCreatePerson: jest.fn(),
   onRemovePerson: jest.fn(),
-  removePersonMutation: jest.fn(() => new Promise(
-    reslove => reslove(),
-  )),
+  showRemove: true,
 }
 
 describe('EditorControls component', () => {
@@ -18,18 +15,16 @@ describe('EditorControls component', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('renders in update mode', () => {
-    const updateProps = { ...props, editMode: 'update' }
-    const wrapper = shallow(<EditorControls {...updateProps} />)
-    expect(wrapper).toMatchSnapshot()
-  })
-
   it('calls removePersonMutation', () => {
-    const updateProps = { ...props, editMode: 'update' }
-    const wrapper = shallow(<EditorControls {...updateProps} />)
+    const wrapper = shallow(<EditorControls {...props} />)
     wrapper.find(Button).at(1).simulate('click')
 
-    expect(props.removePersonMutation).toHaveBeenCalledWith({ id: 1 })
     expect(props.onRemovePerson).toHaveBeenCalled()
+  })
+
+  it('renders in update mode', () => {
+    const newProps = { ...props, showRemove: false }
+    const wrapper = shallow(<EditorControls {...newProps} />)
+    expect(wrapper).toMatchSnapshot()
   })
 })
